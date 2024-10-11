b522f61ad72b034bf42af1673b5566e4f3190434
gpapadog <gpapadogeorgou@gmail.com>
2024-10-11 14:33:28 -0400

    Corrections on interval calculation
    I made the ci_bayes function applicable to BVR, BSC and SMAC. Now we can use the same function for all bayesian methods.
    
    Also, previously, we were giving the STANDARDIZED data to ci_bayes. Therefore, the credible intervals we were getting for ci_bayes were not for the original scale of the data. I corrected this.
    
    This might have to be corrected for the frequentist methods as well.

--- Functions/ci_function.R
+++ Functions/ci_function.R
@@ -11,6 +11,12 @@
 
 ci <- function(sim, est, cal, t0, norm){
   
+  bands = ncol(cal[[1]])
+  
+  # Setting these in case the data are not standardized, norm = FALSE.
+  means <- rep(0, bands)
+  sds <- rep(1, bands)
+  
   # Standardizing the data using the preset function.
   if (norm == T) {
     sim_std <- preset(sim, t0)
@@ -18,10 +24,10 @@ ci <- function(sim, est, cal, t0, norm){
     means <- sim_std$means
     sds <- sim_std$sds
   }
+  # -GP- CAREFUL! From now on, sim is the standardized data!
   
-  bands=ncol(cal[[1]])
-  num_controls=ncol(sim) - bands
-  time_periods=nrow(sim)
+  num_controls = ncol(sim) - bands
+  time_periods = nrow(sim)
   out <- list()
   
   ### 1.0 SEPARATED SCM
@@ -46,42 +52,34 @@ ci <- function(sim, est, cal, t0, norm){
     out$OLS <- ci_shen(sim, estimate, bands, t0)
   }
   
+  # -GP- The CIs for the Bayesian methods below have been corrected to be on
+  # the original scale of the data, NOT standardized.
+  
   ### 4.0 BAYESIAN VERTICAL REGRESSION
   if ("BVR" %in% names(est)) {
     bands <- ncol(cal$BVR)
-    time_periods <- nrow(sim)
-    out$BVR <- ci_bayes(sim, est, "BSC", norm) 
+    out$BVR <- ci_bayes(est_method = est[['BVR']], bands = bands,
+                        means = means, sds = sds) 
   }
   
   ### 5.0 BAYESIAN SYNTHETIC CONTROL
   if ("BSC" %in% names(est)) {
-    time_periods <- nrow(sim)
     bands <- ncol(cal$BSC)
-    out$BSC <- ci_bayes(sim, est, "BSC", norm) 
+    out$BSC <- ci_bayes(est_method = est[["BSC"]], bands = bands, 
+                        means = means, sds = sds) 
   }
   
   
   ### 6.0 SMAC
   if ("SMAC" %in% names(est)) {
-    time_periods <- nrow(sim)
-    bands <- ncol(cal$BSC)
-    lower <- matrix(nrow=time_periods, ncol=bands)
-    upper <- matrix(nrow=time_periods, ncol=bands)
-    for(i in 1 : bands) {
-      lower[, i] <- apply((est[["SMAC"]]$ynn[,,i]*sds[i]) + means[i], 2, 0.025)
-      upper[, i] <- apply((est[["SMAC"]]$ynn[,,i]*sds[i]) + means[i], 2, 0.975)
-      store <- cbind(store, loc)
-    }
-    
-    loc <- list(lower, upper)
-    names(loc) <- c("lower_bound", "upper_bound")
-    out$SMAC <- loc
+    bands <- ncol(cal$SMAC)
+    out$SMAC <- ci_bayes(est_method = est[['SMAC']], bands, 
+                         means = means, sds = sds)
   }
   
   library(purrr)
   out=compact(out)
-  names(out)=names(est)
-  
+
   return(out)
   
 }
--- Methods/helper/ci_bayes.R
+++ Methods/helper/ci_bayes.R
@@ -1,35 +1,46 @@
 #' Bayesian confidence interval
 #' 
-#' @param est the list of posterior distributions from bayes methods
+#' @param est_method the list of posterior distributions from bayes methods
 #' We use this function just for bayesian vertical reg, bayesian synthetic 
-#' control
+#' control, and SMAC.
+#' @param bands The number of treated units.
+#' @param means If the posterior samples for the predictions are standardized,
+#' provide the pre-intervention mean for the treated units. If left equal to
+#' NULL, a vector of 0s will be used, as if the method is fit to data that have
+#' not been standardized.
+#' @param sds If the posterior samples for the predictions are standardized,
+#' provide the pre-intervention standard deviation for the treated units. If
+#' left equal to NULL, a vector of 1s will be used, as if the method is fit to
+#' data that have not been standardized.
 #' 
 #' @autor Giulio Grossi
 #' 
 
 
-ci_bayes <- function(sim, est, method, norm){
+ci_bayes <- function(est_method, bands, means = NULL, sds = NULL) {
   
-  if (norm == T) {
-    sim_std <- preset(sim, t0)
-    sim <- sim_std$std
-    means <- sim_std$means
-    sds <- sim_std$sds
+  if (is.null(means)) means <- rep(0, bands)
+  if (is.null(sds)) sds <- rep(1, bands)
+  
+  lower <- matrix(nrow = time_periods, ncol = bands)
+  upper <- matrix(nrow = time_periods, ncol = bands)
+  
+  for (i in 1 : bands) {
+    
+    # Posterior samples for treated unit i based on the Bayesian methods:
+    
+    if ('ynn' %in% names(est_method)) {  # For SMAC
+      loc <- est_method$ynn[, , i]
+    } else {                             # For BVR and BSC.
+      loc <- est_method[[i]]$y_new
+    }
+    
+    loc <- loc * sds[i] + means[i]  # Re-standardizing them.
+    lower[,i] <- apply(loc, 2, quantile, prob = 0.025)
+    upper[,i] <- apply(loc, 2, quantile, prob = 0.975)
   }
   
-  lower=matrix(nrow=time_periods, ncol=bands)
-  upper=matrix(nrow=time_periods, ncol=bands)
-  
-  for(i in 1:bands){
-    loc <- est[[method]][[i]]$y_new
-    for(hh in 1:nrow(loc)){
-      loc[hh,]=(loc[hh,]*sds[i])+means[i]
-    }	  
-    lower[,i]<-apply(loc,2,quantile, prob=0.025)
-    upper[,i]<-apply(loc,2,quantile, prob=0.975)
-  }
-  
-  out=list(lower, upper) 
-  names(out)=c("lower_bound","upper_bound" )
+  out = list(lower, upper) 
+  names(out) = c("lower_bound","upper_bound" )
   return(out)  
 }
